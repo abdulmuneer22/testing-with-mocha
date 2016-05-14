6cda8254663dcd2756c50a85390c8759e56f200f
Chirag Jain <chirgjn@users.noreply.github.com>
2016-05-14 23:57:25 +0530

    Added test for no-word, multiple-word, and already-capitalized cases

--- test/capitalize.js
+++ test/capitalize.js
@@ -8,10 +8,28 @@ describe('capitalize', function() {
 		expect(capitalize('express')).to.equal('Express');
 		expect(capitalize('cats')).to.equal('Cats');
 	});
+
 	it('makes the rest of the string lowercase', function() {
 		expect(capitalize('javaScript')).to.equal('Javascript');
 	});
+
 	it('leaves empty strings alone', function() {
 		expect(capitalize('')).to.equal('');
+	});
+
+	it('leaves strings with no words alone', function() {
+		expect(capitalize(' ')).to.equal(' ');
+		expect(capitalize('123')).to.equal('123');
+	});
+
+	it('capitalizes multiple-word strings', function() {
+		expect(capitalize('what is Express')).to.equal('What is express');
+		expect(capitalize('i love lamp')).to.equal('I love lamp');
+	});
+
+	it('leaves already-capitalized words alone', function() {
+		expect(capitalize('Express')).to.equal('Express');
+		expect(capitalize('Evan')).to.equal('Evan');
+		expect(capitalize('Catman')).to.equal('Catman');
 	});
 });
